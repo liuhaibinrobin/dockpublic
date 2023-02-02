19dcb982851f3d5861d472ac01e61f39d9cbf97e
yinshiqiu <yinshiqiu@stonewise.cn>
2023-02-02 17:11:05 +0800

    recycling_num=1 只选取 '1wcq','5u7m','2nno','5j87','5uwf','5kxi','4kju','3eta','2b54','1sdt' 作为训练样本
    加入当前构象到真实构象的旋转平移扭转量，直接有监督学习

--- tankbind/torsion_geometry.py
+++ tankbind/torsion_geometry.py
@@ -265,6 +265,9 @@ def modify_conformer_torsion_angles(pos, edge_index, mask_rotate, torsion_update
     torsion_updates=torsion_updates.detach().to("cpu").numpy()
     if type(pos) != np.ndarray: pos = pos.cpu().numpy()
 
+    import pdb
+    pdb.set_trace()
+
     for idx_edge, e in enumerate(edge_index.cpu().numpy()):
         if torsion_updates[idx_edge] == 0:
             continue
@@ -272,8 +275,7 @@ def modify_conformer_torsion_angles(pos, edge_index, mask_rotate, torsion_update
 
         # check if need to reverse the edge, v should be connected to the part that gets rotated
         if mask_rotate[idx_edge, u] or not mask_rotate[idx_edge, v]:
-            import pdb #TODO 这里不太懂
-            pdb.set_trace()
+            # TODO 这里不太懂
             u, v = e[1], e[0]
         # assert not mask_rotate[idx_edge, u]
         # assert mask_rotate[idx_edge, v]
