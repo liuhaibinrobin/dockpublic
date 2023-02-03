1ddb104c1e932dbbf6ab6874b98391a3d50e444a
yinshiqiu <yinshiqiu@stonewise.cn>
2023-02-03 18:25:42 +0800

    recycling_num=1 只选取 '1wcq','5u7m','2nno','5j87','5uwf','5kxi','4kju','3eta','2b54','1sdt' 作为训练样本
    加入当前构象到真实构象的旋转平移扭转量，直接有监督学习
    RMSD准确公式

--- tankbind/main.py
+++ tankbind/main.py
@@ -337,10 +337,10 @@ for epoch in range(10000):
             pdb.set_trace()
 
             rmsd_recycling_0_loss = rmsd_recycling_0_loss/len(data_groundtruth_pos_batched)
-            rmsd_recycling_1_loss = rmsd_list[1] if len(rmsd_list) >= 1 else torch.tensor([0]).to(y_pred.device)
-            rmsd_recycling_9_loss = rmsd_list[9] if len(rmsd_list) >=9 else torch.tensor([0]).to(y_pred.device)
-            rmsd_recycling_19_loss = rmsd_list[19] if len(rmsd_list) >= 19 else torch.tensor([0]).to(y_pred.device)
-            rmsd_recycling_39_loss = rmsd_list[39] if len(rmsd_list) >= 39 else torch.tensor([0]).to(y_pred.device)
+            rmsd_recycling_1_loss = torch.mean(rmsd_list[0]) if len(rmsd_list) >= 1 else torch.tensor([0]).to(y_pred.device)
+            rmsd_recycling_9_loss = torch.mean(rmsd_list[8]) if len(rmsd_list) >=9 else torch.tensor([0]).to(y_pred.device)
+            rmsd_recycling_19_loss = torch.mean(rmsd_list[18]) if len(rmsd_list) >= 19 else torch.tensor([0]).to(y_pred.device)
+            rmsd_recycling_39_loss = torch.mean(rmsd_list[38]) if len(rmsd_list) >= 39 else torch.tensor([0]).to(y_pred.device)
 
             if args.use_weighted_rmsd_loss:
                 prmsd_loss = torch.stack([contact_criterion(rmsd_list[i], prmsd_list[i], args.contact_loss_mode) for i in range(len(prmsd_list))]).mean() if len(prmsd_list) > 0 else torch.tensor([0]).to(y_pred.device)
